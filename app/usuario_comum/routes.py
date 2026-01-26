c4ff51fc47f1d982a40f09cc4021343b8fabc97c
Guilherme Roxo <guilhermegmaf@gmail.com>
2026-01-26 14:14:13 -0300

    chore: adiciona rota GET básica do questionário e altera o nome de 'formulário' para 'questionário' no html e na rota

--- app/usuario_comum/routes.py
+++ app/usuario_comum/routes.py
@@ -1,12 +1,12 @@
 from flask import render_template, request, redirect, url_for, session, flash
 from . import user_bp
 
-@user_bp.route('/formulario', methods=['GET', 'POST'])
-def formulario():
+@user_bp.route('/questionario', methods=['GET', 'POST'])
+def questionario():
     '''
     lógica do formulário
     '''
-    return "formulario"
+    return render_template('questionario.html')
 
 @user_bp.route('/logout')
 def logout_usuario():
